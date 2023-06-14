314ae498a4472d653d1b94e05cb2697e7eff61b9
pepeariel <88797042+pepeariel@users.noreply.github.com>
2023-06-13 23:15:03 -0300

    linux_driver

--- teste.py
+++ teste.py
@@ -1,22 +1,18 @@
-from functions import FileHandler
-from dotenv import load_dotenv
-import os
-import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+CHROMEDRIVER_PATH = 'chromedriver'
+WINDOW_SIZE = "1920,1080"
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+chrome_options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
+                          chrome_options=chrome_options
+                         )
+driver.get("https://www.google.com")
+print(driver.title)
+driver.close()
 
-# Save some random file from excel to parquet format
-excel_file_path = r'/Users/pedroariel/Downloads/relatorio_entregas_historico.xlsx'
-parquet_file_path = r'/Users/pedroariel/Downloads/relatorio_entregas_historico.parquet'
-
-#excel_to_parquet(excel_file_path, parquet_file_path)
-
-# Load local variables
-load_dotenv()
-
-Pipeline = FileHandler()
-
-s3 = Pipeline.create_s3_connection()
-
-files = Pipeline.list_s3_files(s3)
 
 
 
